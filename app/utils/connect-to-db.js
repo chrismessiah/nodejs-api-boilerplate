95c63c55c286bc17a4c8241d04d84dfaff0a789e
Christian Abdelmassih <chrabd@kth.se>
2017-11-19 20:19:14 +0100

    update nodejs and pg

--- app/utils/connect-to-db.js
+++ app/utils/connect-to-db.js
@@ -1,46 +1,18 @@
 'use strict';
 
-let Promise = require('bluebird');
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgresql://root@localhost:5432/db';
+const pool = new Pool({ connectionString: connectionString });
 
-let pg = require("pg");
-let dbConfig = require('pg-connection-string').parse(process.env.DATABASE_URL || 'postgres://root@localhost:5432/db');
-dbConfig.max = 10; dbConfig.idleTimeoutMillis = 3000;
-let pgPool = new pg.Pool(dbConfig);
-
-function makeOneQuery(sql, opts) {
+module.exports = async (sql, opts) => {
   if (opts && opts.showQuery) {console.log(sql);}
-  return new Promise((resolve, reject) => {
-    pgPool.connect((err, client, done) => {
-      if (err) reject();
-      let clientPromise = Promise.promisifyAll(client);
-      clientPromise.query(sql,[])
-      .then((res) => {
-        done();
-        resolve(res.rows);
-      })
-      .catch((err) => {console.log(err);})
-    });
-  });
-}
 
-function makeMultipleQuery(sqlArray, opts, next) {
-    let finalArray = [];
-    return sqlArray.reduce((promise, sql) => {
-        return promise.then(() => {
-          return makeOneQuery(sql).then((result) => {
-            finalArray.push(result);
-            return finalArray;
-          });
-        });
-    },  Promise.resolve());
-
-}
-
-module.exports = function(req, sql, opts) {
-  if (typeof sql === 'string') {
-    return makeOneQuery(sql, opts);
+  let res;
+  try {
+    res = await pool.query(sql, opts);
+    await pool.end()
+  } catch (e) {
+    console.log(e);
   }
-  else if(Object.prototype.toString.call(sql) === '[object Array]') {
-    return makeMultipleQuery(sql, opts);
-  }
+  return res;
 };
